3a7d1effee5ab56831742878be1b3972196f460d
한승우 <peter001019@naver.com>
2025-05-25 20:27:40 +0900

    Silver1 9081 단어 맞추기

--- Implementation/9081 단어 맞추기.py
+++ Implementation/9081 단어 맞추기.py
@@ -5,29 +5,27 @@ ans = []
 
 for w in words:
     w = list(w)
+    found = False
 
     #1. 바뀌는 블록 선택
     for i in range(len(w) - 2, -1, -1): 
         if w[i] < w[i + 1]:
+            found = True
             break
-    
-    if i == 0:
-        ans.append(''.join(w))
-        break
 
-    m = 0
+    if not found:
+        ans.append(''.join(w))
+        continue
 
     #2. 그 다음 문자 찾고 교환
     for j in range(len(w) - 1, i, -1):
         if w[i] < w[j]:
-            m = j
             break
 
-    w[i], w[m] = w[m], w[i]
+    w[i], w[j] = w[j], w[i]
 
     #3. 뒤를 정렬
-    w = w[:i + 1] + w[i + 1:][::-1]
-
+    w = w[:i + 1] + list(reversed(w[i + 1:]))
     ans.append(''.join(w))
 
 for a in ans:
